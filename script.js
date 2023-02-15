b7d5c9e61789840d1804fbccc30bf548e853be2c
ninta <nintah85@gmail.com>
2023-02-15 17:17:49 +0000

    trying functions on JS

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ console.log("hello world");
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 
-btn.addEventListener("click", function()
-{
-	if (form.classList.contains("hidden"))
-	{
-		form.classList.remove("hidden");
-		btn.textContent = "Close";
-	}
-	else
-	{
-		form.classList.add("hidden");
-		btn.textContent = "Share a fact";
-	}
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
 });
+
+let votesInteresting = 23;
+let votesMindblowing = 5;
+const text = "lisbon is the capital of Portugal";
+
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+
+let totalUpvotes = votesInteresting + votesMindblowing;
+console.log(totalUpvotes);
+
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
